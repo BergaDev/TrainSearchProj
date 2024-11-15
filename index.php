d5410fb82fa74be9236c237f79266ea4e61640a1
Matthew <55415791+BergaDev@users.noreply.github.com>
2024-11-16 08:21:44 +1100

    Name color

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['query'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css" />
     <title>Train Search</title>
-    <h1>Matthew Bergamini</h1>
+    <h1><a href="../index.html" class="homeLink">Matthew Bergamini</a></h1>
 </head>
 <body>
     <div class="social-header">
